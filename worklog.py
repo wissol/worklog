7bc2430189e61e2e79631f3ba54156561a304dcd
Miguel de Luis <luis.miguelde@gmail.com>
2016-08-26 17:23:35 +0100

    Work began

---
+++ worklog.py
@@ -0,0 +1,32 @@
+"""
+Work Log, being a script to log the work of a small team on csv files
+
+by Miguel de Luis
+
+Started on 260816
+
+"""
+
+import csv
+
+# constants
+
+WORK_LOG_FILE_NAME = "work_log.csv"
+
+
+def menu():
+    """
+    Shows the main menu, returns the menu option chosen
+    :return:
+    """
+
+
+def main():
+    keep_logging = True
+
+    while keep_logging:
+        option = menu()
+        # do_stuff(option)
+
+
+main()
