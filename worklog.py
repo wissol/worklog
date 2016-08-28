5bb622c1b6a2970e1af15b034a1e5121256afa6c
Miguel de Luis <luis.miguelde@gmail.com>
2016-08-28 08:01:18 +0100

    added validation for time spent on task

--- worklog.py
+++ worklog.py
@@ -16,10 +16,10 @@ from sys import exit
 WORK_LOG_FILE_NAME = "work_log.csv"
 
 MENU_CHOICES = {"a": "add entry", "f": "search entries", "e": "edit entry",
-                "d": "delete entry", "q": "quit", "s": "so_stuff"}
+                "d": "delete entry", "q": "quit"}
 
 """
-MENU_FUNCTIONS = {} will contain the functions called from the menu, yet it's
+MENU_FUNCTIONS = {char:function_name} will contain the functions called from the menu, yet it's
 declared after those functions have been defined
 """
 
@@ -48,11 +48,7 @@ def clear_screen():
     033 = ESC so this is <ESC> + c -> clear terminal
     found on http://askubuntu.com/questions/25077/how-to-really-clear-the-terminal
     """
-    print("\033c", end="")
-
-
-def do_stuff_a():
-    print("stuff a")
+    print("\033c", end="\v")  # adding some white spacea
 
 
 def input_task_notes(task_notes):
@@ -69,16 +65,37 @@ def input_task_notes(task_notes):
         return input_task_notes(task_notes)
 
 
+def input_time_spent(validation_message):
+    """
+    Evaluates a string and returns the time spent in minutes
+    raw_time_spent: string
+    :param validation_message string ... error message
+    :return: integer ... total minutes
+    """
+
+    if validation_message:
+        print("\v\t " + validation_message)
+
+    raw_time_spent = input("Enter time spent, in minutes:> ")
+    if raw_time_spent.isnumeric():
+        try:
+            return int(raw_time_spent)
+        except ValueError:
+            return input_time_spent(validation_message="\aPlease only whole numbers")
+    else:
+        return input_time_spent(validation_message="\aPlease use only whole numbers")
+
 def add_entry():
     """
     Adds an entry
     :return:
     """
     task_description = input("Task Description:> ")
-    time_spent = input("Enter time spent, in minutes")
+    time_spent = input_time_spent("")
     task_notes = input_task_notes([])
 
     print(task_description)
+    print(time_spent)
     print(task_notes)
 
     # update worklog file
@@ -96,7 +113,7 @@ def delete_entry():
     print("delete entry")
 
 MENU_FUNCTIONS = {"a": add_entry, "f": search_entries, "e": edit_entry,
-                  "d": delete_entry, "q": exit, "s": do_stuff_a}
+                  "d": delete_entry, "q": exit}
 
 
 def ask_for_choice(error_message):
